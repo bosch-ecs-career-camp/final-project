41804e4a54d8282d052c1f87706a7df57d283ec2
sashosotirov <sotirov.sasho@gmail.com>
2023-03-11 09:54:01 +0200

    first action

--- test-script.sh
+++ test-script.sh
@@ -4,7 +4,7 @@
 token=$PAT
 orgName=bosch-ecs-career-camp
 repoName=final-project
-imageName=runner
+imageName=runner:v1
 
 
 docker run -e GH_TOKEN=$token -e GH_OWNER=$orgName -e GH_REPOSITORY=$repoName -d $imageName
