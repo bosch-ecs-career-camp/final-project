1d7894c39d5e6db3b0498f0cdd1535b0a8e1461a
sashosotirov <sotirov.sasho@gmail.com>
2023-03-11 09:03:28 +0200

    working Dokerfile

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,17 +5,17 @@ GH_REPOSITORY=$GH_REPOSITORY
 GH_TOKEN=$GH_TOKEN
 
 RUNNER_SUFFIX=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 5 | head -n 1)
-RUNNER_NAME="dockerNode-${RUNNER_SUFFIX}"
+RUNNER_NAME="azureNode-${RUNNER_SUFFIX}"
 
 REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/${GH_OWNER}/${GH_REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 
-cd /home/docker/actions-runner
+cd /home/ghub/actions-runner
 
 ./config.sh --unattended --url https://github.com/${GH_OWNER}/${GH_REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME}
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --unattended --token ${REG_TOKEN}
+    ./config.sh remove --token ${REG_TOKEN}
 }
 
 trap 'cleanup; exit 130' INT
--- test-script.sh
+++ test-script.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+#docker build --build-arg RUNNER_VERSION=2.302.0 .
+token=$PAT
+orgName=bosch-ecs-career-camp
+repoName=final-project
+imageName=runner
 
 
-docker build --build-arg RUNNER_VERSION=2.302.1 .
+docker run -e GH_TOKEN=$token -e GH_OWNER=$orgName -e GH_REPOSITORY=$repoName -d $imageName
