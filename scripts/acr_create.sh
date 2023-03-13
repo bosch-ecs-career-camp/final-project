6cd3238a79b589fe47b437d2214f7c1d2d18c423
sashosotirov <sotirov.sasho@gmail.com>
2023-03-13 13:28:49 +0200

    new build scenario

--- scripts/acr_create.sh
+++ scripts/acr_create.sh
@@ -4,9 +4,9 @@ export location="westeurope"
 export resourceGroup="final-project"
 export acrName=stream4bosch
 
+acrLength=`az acr list --query "[?name=='stream4bosch']"`
+[[ ${#acrLength} -gt 3 ]] && echo "acr exist" || \
 az acr create --resource-group $resourceGroup \
   --name $acrName --sku Basic
-
-az acr login --name $acrName
 
 
