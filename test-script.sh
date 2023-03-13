d6fa33456b8fc76e153c2eef3bdcb11451001dac
sashosotirov <sotirov.sasho@gmail.com>
2023-03-13 13:32:07 +0200

    new build scenario

--- test-script.sh
+++ test-script.sh
@@ -8,3 +8,13 @@ imageName=runner:v1
 
 
 docker run -e GH_TOKEN=$token -e GH_OWNER=$orgName -e GH_REPOSITORY=$repoName -d $imageName
+
+
+res=`az acr list --query "[?name=='stream4bosch']"`
+if [[ ${#res} -gt 3 ]]; then echo ok ; fi
+
+
+acrLength=`az acr list --query "[?name=='stream4bosvch']"`
+[[ ${#acrLength} -gt 3 ]] && echo "acr exist" || \
+az acr create --resource-group $resourceGroup \
+  --name $acrName --sku Basic
