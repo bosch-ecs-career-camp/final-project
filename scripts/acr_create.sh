4b6b73f4b2ee1595d5e4aab4aa90eb38c778ef8a
sashosotirov <sotirov.sasho@gmail.com>
2023-03-15 11:55:18 +0200

    update workflow

--- scripts/acr_create.sh
+++ scripts/acr_create.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 export location="westeurope"
-export resourceGroup="images"
+export resourceGroup="final-project"
 export acrName=stream4bosch
 
 acrLength=`az acr list --query "[?name=='stream4bosch']"`
 [[ ${#acrLength} -gt 3 ]] && echo "acr exist" || \
 az acr create --resource-group $resourceGroup \
-  --name $acrName --sku Basic
+  --name $acrName --sku Basic \
+  --admin-enabled true 
 
 
