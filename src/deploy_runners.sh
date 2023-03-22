ed01c06251825c58cf6302267d1789241893fdd1
sashosotirov <sotirov.sasho@gmail.com>
2023-03-22 21:37:58 +0200

    share tag by artifacts

--- src/deploy_runners.sh
+++ src/deploy_runners.sh
@@ -9,10 +9,10 @@ az aks get-credentials --resource-group stream4-rg \
 # ----  ACTION RUNNER CONTROLLER ----
 
 # ----  install cert-manager for enabling admission web-hooks ---- 
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.11.0/cert-manager.yaml
+kubectl apply -f cert-manager.yaml
 
 # ---- install and setup action runner controller ----
-kubectl create -f https://github.com/actions/actions-runner-controller/releases/download/v0.27.0/actions-runner-controller.yaml
+kubectl create -f actions-runner-controller.yaml
 
 # ----	enable authentication to GitHub API ----
 kubectl create secret generic controller-manager \
