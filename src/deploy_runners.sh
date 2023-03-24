0dc9e8ae33d093251cde34911b20ea712163bd9e
sashosotirov <sotirov.sasho@gmail.com>
2023-03-24 18:18:52 +0200

    start using github runner

--- src/deploy_runners.sh
+++ src/deploy_runners.sh
@@ -24,5 +24,5 @@ kubectl create secret generic controller-manager \
 kubectl create -f actions-runner-controller.yaml
 
 # ----  deploying ARC runners ----
-sleep 120
+sleep 30
 cat runner_deployment.yml | envsubst | kubectl apply -f -
